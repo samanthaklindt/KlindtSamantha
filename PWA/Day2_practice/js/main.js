489b2bd4df30b3ae07d0ad9ae87ceefe06b40481
samanthaklindt <samanthaklindt@Samanthas-MacBook-Pro-3.local>
2014-02-06 12:10:13 -0500

    Lecture Notes - Day 2
    notes from class

--- PWA/Day2_practice/js/main.js
+++ PWA/Day2_practice/js/main.js
@@ -25,3 +25,33 @@ helloworld2();                          //calling name function
 //Have to know both functions, but pick the one you think is best as a student to use.
 
 
+//Strings
+
+(function(){
+    var name = "Full Sail University";
+    var 1 = name.charAt(10);            //.charAt(10) - Gives the character of the specified index
+
+
+    var r = name.indexOf("S");          //.indexOf() - Gives the index number of the given string
+
+    var t = name.length;                //.length - Gives the length of the given string
+
+    var splitTest = "I love JavaScript much!";
+    var words = splitTest.split(" ")    //.split - Gives whatever sort of spacing to the string
+
+    console.log(splitTest);             //Converts to array
+
+    console.log(words);                 //Prints the list into array form
+
+    console.log(words[2]);              //Prints out the index of 2 = JavaScript
+
+    var piece = name.substr(5,4);       //.substr - Gives the info from the asked for arguement
+
+    var anotherPiece = name.substring(5,8); //.substring - Gives the info from the asked for arguement
+
+    console.log(piece);
+
+    console.log(anotherPiece);
+
+
+})();
